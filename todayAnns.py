2563e481ceb59b758e053fe923198e11babdcc90
Aaron Ash <aaron.ash@gmail.com>
2023-08-12 11:26:49 +1000

    misc: eat errors in todaysAnns.py

--- todayAnns.py
+++ todayAnns.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from datetime import datetime
 
-dfs = pd.read_html('https://www.asx.com.au/asx/v2/statistics/todayAnns.do')
-if len(dfs) > 0:
-    df = dfs[0]
-    today = df['Date'].iloc[0]
-    today = datetime.strptime(today, '%d/%m/%Y %I:%M %p').date().isoformat()
-    df.to_csv(f"todayAnns_{today}.csv", index=False)
+try:
+    dfs = pd.read_html('https://www.asx.com.au/asx/v2/statistics/todayAnns.do')
+    if len(dfs) > 0:
+        df = dfs[0]
+        today = df['Date'].iloc[0]
+        today = datetime.strptime(today, '%d/%m/%Y %I:%M %p').date().isoformat()
+        df.to_csv(f"todayAnns_{today}.csv", index=False)
 
+except:
+    pass # fail silently so the github action doesnt fail unneccesarily
